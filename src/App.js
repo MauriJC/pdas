5f92f200129b7ab6d324b813e4941073d3c7b5e2
Mauricio Chaile <xaviermauricio33@gmail.com>
2023-03-19 18:44:43 -0300

    Addes description to ProfileCard component and header got formatted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png'
-import SiriImage from './images/siri.png'
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+import 'bulma/css/bulma.css';
+// when importing from node modules we dont have to put a relative path
 
 
-//hasta el 36 son 30 min
-// hasta el 41 los otros 30
+//hasta el 38 son 30 min
+// hasta el 44 los otros 30
 
 const App = () => {
   return (
     <div>
-      <img src={AlexaImage} alt="" />
-        <div>Personal Digital Assistants</div>
-        <ProfileCard title='Alexa ' handle='@alexa99'></ProfileCard>
-        <ProfileCard title='Cortana' handle='@cortana32'></ProfileCard>
-        <ProfileCard title='Siri' handle='@siri01'></ProfileCard>
+        
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <div className="title">Personal Digital Assistants</div>
+
+          </div>
+        </section>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-3">
+               <ProfileCard image={AlexaImage} title='Alexa' handle='@alexa99' description='Alexa was created by amazon and helps you buy things'></ProfileCard>
+              </div>
+              <div className="column is-3"></div>
+                <ProfileCard image={CortanaImage} title='Cortana' handle='@cortana32' description='Cortana was made by microsoft. Who knows what it does?'></ProfileCard>
+              <div className="column is-3">
+                <ProfileCard image={SiriImage} title='Siri' handle='@siri01' description='Siri was made by Apple and is being phased out'></ProfileCard>
+              </div>
+            </div>
+          </section>
+        </div>
+        
+    
+
       
     </div>
   )
