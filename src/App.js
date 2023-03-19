a84e8e59b8280560f681ecbb805f792958d75a59
Mauricio Chaile <xaviermauricio33@gmail.com>
2023-03-19 11:49:00 -0300

    added Images and handles to profilecard components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+
+
+//hasta el 36 son 30 min
+// hasta el 41 los otros 30
 
 const App = () => {
   return (
     <div>
+      <img src={AlexaImage} alt="" />
         <div>Personal Digital Assistants</div>
         <ProfileCard title='Alexa ' handle='@alexa99'></ProfileCard>
         <ProfileCard title='Cortana' handle='@cortana32'></ProfileCard>
